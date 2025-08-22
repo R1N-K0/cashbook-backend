d4c0b0c53d6412972c49aa2de0c26928cc8ccb97
R1N-K0 <peanut904018@gmail.com>
2025-08-22 15:00:18 +0900

    setting:prettierとHuskyの設定

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,60 @@
-// @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
-export default tseslint.config(
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({ baseDirectory: __dirname });
+
+const eslintConfig = [
+
+  ...compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier"
+  ),
+
+
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: ["node_modules/**", "dist/**", "build/**"]
   },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
+
   {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      sourceType: 'commonjs',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
-  {
+    files: ["**/*.ts", "**/*.js"],
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
-    },
-  },
-);
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        { prefer: "type-imports", fixStyle: "separate-type-imports" }
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { vars: "all", varsIgnorePattern: "^_", args: "after-used", argsIgnorePattern: "^_" }
+      ],
+      "object-shorthand": "error",
+      "sort-keys-fix/sort-keys-fix": "error",
+      "typescript-sort-keys/interface": "error",
+      "unused-imports/no-unused-imports": "error",
+      "import/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+            "object",
+            "type"
+          ],
+          "newlines-between": "always",
+          alphabetize: { order: "asc" }
+        }
+      ],
+      "import/no-duplicates": "error"
+    }
+  }
+];
+
+export default eslintConfig;
