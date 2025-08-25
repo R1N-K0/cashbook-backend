0ace2c7347ee8ec9cedf23bae95542a8fc88da5b
R1N-K0 <peanut904018@gmail.com>
2025-08-25 16:46:42 +0900

    feat: localStrategyの実装

---
+++ src/auth/local.strategy.ts
@@ -0,0 +1,28 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common'
+import { PassportStrategy } from '@nestjs/passport'
+import { Strategy as BaseLocalStrategy } from 'passport-local'
+
+import type { AuthService } from 'src/auth/auth.service'
+import type { Users } from 'src/entities/users.entity'
+
+type PasswordOmitUser = Omit<Users, 'password'>
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(BaseLocalStrategy) {
+  constructor(private readonly authService: AuthService) {
+    super()
+  }
+
+  async validate(
+    name: Users['name'],
+    pass: Users['password'],
+  ): Promise<PasswordOmitUser> {
+    const user = await this.authService.validateUser(name, pass)
+
+    if (!user) {
+      throw new UnauthorizedException('認証に失敗しました')
+    }
+
+    return user
+  }
+}
