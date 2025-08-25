a78d8d284fee23c148acb324be0b861036a81c89
R1N-K0 <peanut904018@gmail.com>
2025-08-25 17:25:46 +0900

    feat: authModuleの実装

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,26 @@
 import { Module } from '@nestjs/common'
+import { JwtModule } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport'
+import { jwtConstants } from 'src/auth/constants'
+import { UsersModule } from 'src/users/users.module'
 
 import { AuthService } from './auth.service'
 
 @Module({
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      useFactory: async () => {
+        return {
+          secret: jwtConstants.secret,
+          signOptions: {
+            expiresIn: jwtConstants.expiresIn,
+          },
+        }
+      },
+    }),
+  ],
   providers: [AuthService],
 })
 export class AuthModule {}
