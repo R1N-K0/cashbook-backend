c44c4415501a29e9d66e94bfe9070373764c9e7c
R1N-K0 <peanut904018@gmail.com>
2025-08-27 14:43:01 +0900

    feat: CRUDの基本実装

--- src/closing-logs/closing-logs.service.ts
+++ src/closing-logs/closing-logs.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, NotFoundException } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { RequestUser } from 'src/auth/types/request-user'
 import { ClosingLogs } from 'src/entities/closing_log.entity'
@@ -13,9 +13,33 @@ export class ClosingLogsService {
     private readonly usersService: UsersService,
   ) {}
 
-  async create(user: RequestUser) {}
+  async create(user: RequestUser) {
+    const loginUser = await this.usersService.findOneById(user.id)
+    if (!loginUser) throw new NotFoundException('ユーザーが存在しません')
 
-  async findAll() {}
+    const now = new Date('2022/08/25')
+    const yyyy = String(now.getFullYear())
+    const mm = String(now.getMonth() + 1).padStart(2, '0')
+    const dd = String(now.getDate()).padStart(2, '0')
 
-  async findOne(id: number) {}
+    const date = `${yyyy}-${mm}-${dd}`
+
+    const closingLog = this.LogsRepository.create({
+      closingDate: date,
+      user: loginUser,
+    })
+
+    return await this.LogsRepository.save(closingLog)
+  }
+
+  async findAll() {
+    return await this.LogsRepository.find()
+  }
+
+  async findOne(id: number) {
+    const closingLog = await this.LogsRepository.findOneBy({ id })
+    if (!closingLog) throw new NotFoundException('データが存在しません')
+
+    return closingLog
+  }
 }
