2d2f435134555b5304dbb1fee8db731244fe0251
R1N-K0 <peanut904018@gmail.com>
2025-08-28 13:42:43 +0900

    feat: receiptのドキュメント追加

--- src/receipts/receipts.controller.ts
+++ src/receipts/receipts.controller.ts
@@ -7,6 +7,10 @@ import {
   UseGuards,
 } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
+import {
+  ApiCreateReceipt,
+  ApiRemoveReceipt,
+} from 'src/receipts/decorators/swagger-receipt.decorator'
 import { CreateReceiptDto } from 'src/receipts/dto/create-receipt.dto'
 import { ReceiptsService } from 'src/receipts/receipts.service'
 
@@ -16,11 +20,13 @@ export class ReceiptsController {
   constructor(private readonly receiptsService: ReceiptsService) {}
 
   @Post()
+  @ApiCreateReceipt()
   async create(@Body() createReceiptDto: CreateReceiptDto) {
     return await this.receiptsService.create(createReceiptDto)
   }
 
   @Delete(':id')
+  @ApiRemoveReceipt()
   async remote(@Param('id') id: number) {
     return await this.receiptsService.remove(id)
   }
