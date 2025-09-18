c7c8f4750c71a329d0f7c7cecc86414845f15186
R1N-K0 <peanut904018@gmail.com>
2025-09-18 11:38:01 +0900

    feat: add-migrations

---
+++ src/migrations/1758162690156-fix_createdUser.ts
@@ -0,0 +1,49 @@
+import type { MigrationInterface, QueryRunner } from 'typeorm'
+
+export class FixCreatedUser1758162690156 implements MigrationInterface {
+  name = 'FixCreatedUser1758162690156'
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "transaction_users" ("id" BIGSERIAL NOT NULL, "last_name" character varying(255) NOT NULL, "first_name" character varying(255) NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_9faac6f06d4a926e92996b27448" PRIMARY KEY ("id"))`,
+    )
+    await queryRunner.query(
+      `CREATE TABLE "transactions" ("id" BIGSERIAL NOT NULL, "date" date NOT NULL, "description" character varying NOT NULL, "memo" text, "amount" integer NOT NULL, "editable" boolean NOT NULL DEFAULT true, "deletedDate" TIMESTAMP, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "user_id" bigint, "created_user_id" bigint, "updated_user_id" bigint, "category_id" bigint, CONSTRAINT "PK_a219afd8dd77ed80f5a862f1db9" PRIMARY KEY ("id"))`,
+    )
+    await queryRunner.query(
+      `ALTER TABLE "transactions" ADD CONSTRAINT "FK_e9acc6efa76de013e8c1553ed2b" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    )
+    await queryRunner.query(
+      `ALTER TABLE "transactions" ADD CONSTRAINT "FK_dcb44d6d0e3f121749c27ccf0dd" FOREIGN KEY ("created_user_id") REFERENCES "transaction_users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    )
+    await queryRunner.query(
+      `ALTER TABLE "transactions" ADD CONSTRAINT "FK_472989ddac60450f0fc3eb60db5" FOREIGN KEY ("updated_user_id") REFERENCES "transaction_users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    )
+    await queryRunner.query(
+      `ALTER TABLE "transactions" ADD CONSTRAINT "FK_c9e41213ca42d50132ed7ab2b0f" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    )
+    await queryRunner.query(
+      `ALTER TABLE "transactions_receipts" ADD CONSTRAINT "FK_51565c480b9c61779adf9ef4161" FOREIGN KEY ("transaction_id") REFERENCES "transactions"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "transactions_receipts" DROP CONSTRAINT "FK_51565c480b9c61779adf9ef4161"`,
+    )
+    await queryRunner.query(
+      `ALTER TABLE "transactions" DROP CONSTRAINT "FK_c9e41213ca42d50132ed7ab2b0f"`,
+    )
+    await queryRunner.query(
+      `ALTER TABLE "transactions" DROP CONSTRAINT "FK_472989ddac60450f0fc3eb60db5"`,
+    )
+    await queryRunner.query(
+      `ALTER TABLE "transactions" DROP CONSTRAINT "FK_dcb44d6d0e3f121749c27ccf0dd"`,
+    )
+    await queryRunner.query(
+      `ALTER TABLE "transactions" DROP CONSTRAINT "FK_e9acc6efa76de013e8c1553ed2b"`,
+    )
+    await queryRunner.query(`DROP TABLE "transactions"`)
+    await queryRunner.query(`DROP TABLE "transaction_users"`)
+  }
+}
